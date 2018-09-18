dc8345f9f6562ead6a849596148559a6ecfd4516
qheldiv <qheldiv@gmail.com>
2018-09-18 12:56:19 +0800

    edited .gitignore

--- setup.bash
+++ setup.bash
@@ -1,5 +1,5 @@
 export VCCHUSKY_DIR=$(pwd)
 source ~/.bashrc
 source ${VCCHUSKY_DIR}/cartographer_ws/install_isolated/setup.bash
-source ${VCCHUSKY_DIR}/husky_ws/devel/setup.bash
+source ${VCCHUSKY_DIR}/husky_ws/devel/setup.bash --extend
 
